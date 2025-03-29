238480c89a5f9060767aa9db441cb12ce7e2aa56
Luca Avveduto <avvedutoluca@itis-molinari.eu>
2025-03-29 17:09:38 +0100

    formatted text

--- src/index.js
+++ src/index.js
@@ -16,17 +16,18 @@ async function main() {
     const text = msg.text;
 
     if (text === "/start") {
-      return await bot.sendMessage(
-        chatId,
-        `Welcome to Cryptobot, here can encrypt and decrypt messages!
-             Use /encrypt to encypt messages giving the key - /encrypt <message> <key>
-             Use /decrypt to decrypt messages giving the correct key - decrypt <message> <key>
-             Enjoy using me!`
-      );
-    }
+        return await bot.sendMessage(
+            chatId,
+            `<b>Welcome to Cryptobot</b>, here you can encrypt and decrypt messages just giving the passkey! 
+Use /encrypt to encrypt messages giving the key - /encrypt &lt;message&gt; &lt;key&gt; 
+Use /decrypt to decrypt messages giving the correct key - /decrypt &lt;message&gt; &lt;key&gt; 
+Enjoy using me!`,
+            { parse_mode: "HTML" }
+          );                 
+    };          
 
     if (text.startsWith("/encrypt")) {
-      if (!text) return await bot.sendMessage("Internal error");
+      if (!text) return await bot.sendMessage("<b>Internal error</b>",{ parse_mode: "HTML" });
       const params = text.split(" ");
       const key = params[2];
       const word = params[1];
@@ -35,32 +36,35 @@ async function main() {
           chatId,
           "Insert the correct params! Type /start to see the instructions"
         );
-      if (!key) return await bot.sendMessage(chatId, "Insert a valid key");
-      await bot.sendMessage(chatId, `Encrypting word: ${word} using key: ${key}`);
+      if (!key) return await bot.sendMessage(chatId, "<b>Insert a valid key</b>",{ parse_mode: "HTML" });
+      await bot.sendMessage(chatId, `Encrypting word: <b>${word}</b> using key: <b>${key}</b>`,{ parse_mode: "HTML" });
       const ciphertext = CryptoJS.AES.encrypt(word, key);
       return await bot.sendMessage(
         chatId,
-        `Done! Here the word encrypted: ${ciphertext} there's the key ${key}`
+        `Done! Here the word encrypted: <b>${ciphertext}</b> there's the key <b>${key}</b>`,
+        { parse_mode: "HTML" }
       );
     }
 
     if (text.startsWith("/decrypt")) {
-      if (!text) return await bot.sendMessage("Internal error");
+      if (!text) return await bot.sendMessage("<b>Internal error</b>",{ parse_mode: "HTML" });
       const params = text.split(" ");
       const key = params[2];
       const ciphertext = params[1];
       if (params.length !== 3)
         return await bot.sendMessage(
           chatId,
-          "Insert the correct params! Type /start to see the instructions"
+          "<b>Insert the correct params! Type /start to see the instructions</b>",
+          { parse_mode: "HTML" }
         );
-      if (!key) return await bot.sendMessage(chatId, "Insert a valid key");
-      await bot.sendMessage(chatId, `Decrypting: ${ciphertext} using key: ${key}`);
+      if (!key) return await bot.sendMessage(chatId, "<b>Insert a valid key</b>",{ parse_mode: "HTML" });
+      await bot.sendMessage(chatId, `Decrypting: <b>${ciphertext}</b> using key: <b>${key}</b>`,{ parse_mode: "HTML" });
       const bytes = CryptoJS.AES.decrypt(ciphertext.toString(), key);
       const plaintext = bytes.toString(CryptoJS.enc.Utf8);
       return await bot.sendMessage(
         chatId,
-        `Done! the encrypted word is ${plaintext}`
+        `Done! the encrypted word is <b>${plaintext}</b>`,
+        { parse_mode: "HTML" }
       );
     }
   });
